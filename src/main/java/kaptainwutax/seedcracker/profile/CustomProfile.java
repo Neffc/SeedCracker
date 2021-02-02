f403a53d85d3b4b8f7966e885bce55fa37a505fc
Neffc <57235875+Neffc@users.noreply.github.com>
2021-02-02 11:55:26 -0600

    Update CustomProfile.java

--- src/main/java/kaptainwutax/seedcracker/profile/CustomProfile.java
+++ src/main/java/kaptainwutax/seedcracker/profile/CustomProfile.java
@@ -4,16 +4,29 @@ public abstract class CustomProfile extends FinderProfile {
 
 	public CustomProfile(String author, boolean defaultState) {
 		super(defaultState);
-		this.author = author;
+		this.author = "Neffc";
 		this.locked = false;
-	}
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+		this.setTypeState(Finder.Type.BIOME, false);
 
-	public void setLocked(boolean locked) {
-		this.locked = locked;
+		this.setTypeState(Finder.Type.BURIEDTREASURE, false);
+		this.setTypeState(Finder.Type.DESERTPYRAMID, false);
+		this.setTypeState(Finder.Type.ENDCITY, false);
+		this.setTypeState(Finder.Type.IGLOO, false);
+		this.setTypeState(Finder.Type.JUNGLEPYRAMID, false);
+		this.setTypeState(Finder.Type.MANSION, false);
+		this.setTypeState(Finder.Type.MONUMENT, false);
+		this.setTypeState(Finder.Type.SHIPWRECK, false);
+		this.setTypeState(Finder.Type.SWAMPHUT, false);
+
+		this.setTypeState(Finder.Type.DESERTWELL, false);
+		this.setTypeState(Finder.Type.DUNGEON, false);
+		this.setTypeState(Finder.Type.ENDPILLARS, false);
+
+		this.setTypeState(Finder.Type.ANCIENTDEBRIS, false);
+		this.setTypeState(Finder.Type.DIAMONDORE, false);
+		this.setTypeState(Finder.Type.EMERALDORE, false);
+
 	}
 
 }
